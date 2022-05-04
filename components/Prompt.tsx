44d5e07f2c05615a406770949ee5486ee0d6f295
Claire Nord <claire.nord@samsara.com>
2022-05-04 15:26:17 -0700

    components prompt: show timer bar across clue for reading
    Approximate the amount of time it takes to read a clue by its length in
    characters. In the future we could only allow buzzing after the reading is
    done.

--- components/Prompt.tsx
+++ components/Prompt.tsx
@@ -1,15 +1,35 @@
 import { Clue, } from "../pages/api/gameResponse";
 import styles from "../styles/Prompt.module.css";
 import cn from "classnames";
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 interface Props {
 	clue?: Clue;
 	onClick: () => void;
 }
 
+/** MS_PER_CHARACTER is a heuristic value to scale the amount of time per clue by
+ * its length.
+ */
+const MS_PER_CHARACTER = 50;
+
 export default function Prompt(props: Props) {
 	const [clicked, setClicked] = useState(false);
+	const now = Date.now();
+	const [start, setStart] = useState(now);
+	const [progress, setProgress] = useState(now);
+
+	useEffect(() => {
+		const now = Date.now();
+		setStart(now);
+		setProgress(now);
+		if (props.clue) {
+			const interval = setInterval(() => {
+				setProgress(Date.now());
+			}, 1000);
+			return () => clearInterval(interval);
+		}
+	}, [props.clue]);
 
 	const handleClick = () => {
 		if ((props.clue?.isFinal || props.clue?.isDailyDouble) && !clicked) {
@@ -18,6 +38,7 @@ export default function Prompt(props: Props) {
 		}
 		props.onClick();
 	}
+
 	const renderContent = () => {
 		if (props.clue?.isDailyDouble && !clicked) {
 			return "Daily Double";
@@ -28,7 +49,23 @@ export default function Prompt(props: Props) {
 		}
 	};
 
+	const calculateWidth = () => {
+		const numCharactersInClue = props.clue?.clue.length ?? 0;
+		const clueDurationMS = MS_PER_CHARACTER * numCharactersInClue;
+		const diff = progress - start;
+		if (diff <= 0) {
+			return 0;
+		} else if (diff > clueDurationMS) {
+			return 100;
+		}
+		return (diff / clueDurationMS) * 100;
+	}
+	const width = calculateWidth();
+
 	return <div className={cn(styles.prompt, {
 		[styles.isActive]: props.clue !== undefined,
-	})} onClick={handleClick}>{renderContent()}</div>;
+	})} onClick={handleClick}>
+		<div className={styles.text}><span>{renderContent()}</span></div>
+		<div className={styles.timer} style={{ width: `${width}%` }} />
+	</div>;
 }
