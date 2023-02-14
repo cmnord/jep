2b45c1f53892339b249c6c72ccb7525b8431033f
Claire Nord <cnord@alum.mit.edu>
2023-02-13 23:38:40 -0800

    app routes: wip test

--- app/routes/$boardId/game/$gameId.tsx
+++ app/routes/$boardId/game/$gameId.tsx
@@ -10,10 +10,18 @@ export async function loader({ params }: LoaderArgs) {
   const boardId = params.boardId;
   const gameId = params.gameId;
 
-  const fullPath = path.join(process.cwd(), "app/static/mock.jep.json");
-  const file = await fs.promises.readFile(fullPath);
+  // Find the absolute path of the json directory
+  // Note: Vercel doesn't include the json directory when using process.cwd() or
+  // path.join(). The workaround is to use __dirname and concatenate the json
+  // directory to it.
+  const jsonDirectory = __dirname + "/../app/static";
+  // Read the json data file data.json
+  const fileContents = await fs.promises.readFile(
+    jsonDirectory + "/mock.jep.json",
+    "utf8"
+  );
 
-  const game = Convert.toGame(file.toString());
+  const game = Convert.toGame(fileContents);
 
   return json({
     boardId,
