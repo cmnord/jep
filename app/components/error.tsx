2da1a0d958b10d6592b63e21821f4a8d071b131f
Claire Nord <cnord@alum.mit.edu>
2023-02-14 03:50:57 -0800

    app components error: print stack better

--- app/components/error.tsx
+++ app/components/error.tsx
@@ -1,10 +1,16 @@
 export default function Error({ error }: { error: Error }) {
+  const stack = error.stack ? error.stack.split("\n") : [];
+
   return (
     <div className="p-12">
       <h1 className="text-xl font-bold mb-4">Error :(</h1>
       <div className="bg-red-100 rounded-md p-3">
         <p className="text-sm text-red-500 mb-2 font-mono">{error.message}</p>
-        <p className="text-sm text-red-500 font-mono">{error.stack}</p>
+        {stack.map((s, i) => (
+          <p key={i} className="ml-4 text-sm text-red-500 font-mono">
+            {s}
+          </p>
+        ))}
       </div>
     </div>
   );
