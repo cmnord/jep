702a0c9063c0ca1f2dce23efb248dcf3a1f457c2
Claire Nord <cnord@alum.mit.edu>
2023-02-14 00:51:20 -0800

    app components modal: bounce in animation

--- app/components/modal.tsx
+++ app/components/modal.tsx
@@ -27,7 +27,7 @@ export default function Modal(props: {
       <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
       <div className="fixed inset-0 z-10 overflow-y-auto">
         <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-          <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 w-full sm:max-w-lg">
+          <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 w-full sm:max-w-lg animate-bouncein">
             <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
               <div className="sm:flex sm:items-start">
                 <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
