540f4ae9e262fe19156ce20fa316f64119716a27
Claire Nord <cnord@alum.mit.edu>
2023-02-14 23:42:08 -0800

    app routes index: add date picker to play game from a date

--- app/components/button.tsx
+++ app/components/button.tsx
@@ -3,6 +3,7 @@ import classNames from "classnames";
 interface Props {
   onClick?: () => void;
   type?: "primary" | "default";
+  disabled?: boolean;
   htmlType?: React.ButtonHTMLAttributes<HTMLButtonElement>["type"];
   children: React.ReactNode;
 }
@@ -11,15 +12,20 @@ export default function Button({
   onClick,
   children,
   type = "default",
+  disabled = false,
   htmlType,
 }: Props) {
   return (
     <button
+      disabled={disabled}
       onClick={onClick}
       className={classNames(
-        "inline-flex w-full justify-center rounded-md border px-4 py-2 text-base font-medium shadow-sm transition-colors focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 sm:w-auto sm:text-sm",
+        "inline-flex w-full justify-center rounded-md border px-4 py-2 text-base font-medium shadow-sm transition-colors " +
+          "focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 " +
+          "sm:w-auto sm:text-sm " +
+          "disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none disabled:cursor-not-allowed",
         {
-          "border-transparent bg-blue-600 text-white hover:bg-blue-700 ":
+          "border-transparent bg-blue-600 text-white hover:bg-blue-700":
             type === "primary",
           "border-blue-600 text-blue-600 hover:text-blue-700 hover:border-blue-700":
             type === "default",
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,5 +1,6 @@
 import { ActionArgs, redirect } from "@remix-run/node";
 import { Form, Link, useTransition } from "@remix-run/react";
+import * as React from "react";
 
 import { fetchRandomCategories } from "~/models/cluebase.server";
 import { makeGameId } from "~/utils/utils";
@@ -20,6 +21,13 @@ export async function action({ request }: ActionArgs) {
 export default function Index() {
   const transition = useTransition();
 
+  // YYYY-MM-DD format
+  const [date, setDate] = React.useState("");
+
+  const handleChangeDate = (d: string) => {
+    setDate(d);
+  };
+
   return (
     <div className="p-12">
       <Anchor to="https://j-archive.com">J! Archive &rarr;</Anchor>
@@ -27,20 +35,39 @@ export default function Index() {
         Visit the J! Archive home page itself to find episode dates.
       </p>
       <h2 className="text-2xl font-semibold mb-4">Games</h2>
-      <div className="flex flex-col gap-4 flex-wrap items-start">
-        <Button>
-          <Link to={"/1/play"}>Play an example game</Link>
-        </Button>
+      <div className="flex flex-col gap-4 items-start">
         <Button>
           <Link to={"/game/mock"}>Play a mock game</Link>
         </Button>
-        <Form method="post">
+        <Form method="post" className="flex flex-col gap-4 items-start w-full">
           <Button type="primary" htmlType="submit">
             {transition.state === "loading"
               ? "Loading..."
               : "Play a random game from Cluebase"}
           </Button>
         </Form>
+        <div className="flex gap-4">
+          <div className="relative max-w-sm">
+            <input
+              value={date}
+              onChange={(e) => handleChangeDate(e.target.value)}
+              name="date"
+              type="date"
+              className={
+                "block w-full rounded-md border px-4 py-2 text-base font-medium shadow-sm transition-colors " +
+                "focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 " +
+                "sm:w-auto sm:text-sm " +
+                "border-blue-600 text-slate-600 hover:text-slate-700 hover:border-blue-700"
+              }
+              placeholder="Select date"
+            />
+          </div>
+          <Link to={`/${date}/play`}>
+            <Button disabled={date === ""}>
+              Play a game from {date ? date : "a specific air date"}
+            </Button>
+          </Link>
+        </div>
       </div>
     </div>
   );
