56838abd861ea6a5ae614fa0d9bc81f440c04039
Claire Nord <cnord@alum.mit.edu>
2023-02-15 15:15:39 -0800

    app models game.server: move getMockGame to server

--- app/models/game.server.ts
+++ app/models/game.server.ts
@@ -1,8 +1,10 @@
-/* Reads */
-
+import { readFile } from "fs/promises";
 import { get, ref } from "firebase/database";
+
 import { db } from "~/firebase.server";
-import { Convert, Game } from "./convert.server";
+import { Convert, Game } from "~/models/convert.server";
+
+/* Reads */
 
 export async function getAllGames() {
   const games: Game[] = [];
@@ -19,6 +21,19 @@ export async function getAllGames() {
   }
 
   return games;
+}
+
+export async function getMockGame() {
+  // Find the absolute path of the json directory
+  // Note: Vercel doesn't include the json directory when using process.cwd() or
+  // path.join(). The workaround is to use __dirname and concatenate the json
+  // directory to it.
+  const jsonDirectory = __dirname + "/../app/static";
+  // Read the json data file data.json
+  const fileContents = await readFile(jsonDirectory + "/mock.jep.json", "utf8");
+
+  const game = Convert.toGame(fileContents);
+  return game;
 }
 
 /* Writes */
--- app/routes/game.mock.tsx
+++ app/routes/game.mock.tsx
@@ -1,23 +1,11 @@
 import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
-import fs from "fs";
 
 import GameComponent from "~/components/game";
-import { Convert } from "~/models/convert.server";
+import { getMockGame } from "~/models/game.server";
 
 export async function loader() {
-  // Find the absolute path of the json directory
-  // Note: Vercel doesn't include the json directory when using process.cwd() or
-  // path.join(). The workaround is to use __dirname and concatenate the json
-  // directory to it.
-  const jsonDirectory = __dirname + "/../app/static";
-  // Read the json data file data.json
-  const fileContents = await fs.promises.readFile(
-    jsonDirectory + "/mock.jep.json",
-    "utf8"
-  );
-
-  const game = Convert.toGame(fileContents);
+  const game = await getMockGame();
 
   return json({ game });
 }
