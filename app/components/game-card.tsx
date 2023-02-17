f44c6870edab06bcae0c244ff97fd8828afc994a
Claire Nord <cnord@alum.mit.edu>
2023-02-16 18:16:15 -0800

    app components game-card: handle overflowing title & author

--- app/components/game-card.tsx
+++ app/components/game-card.tsx
@@ -14,17 +14,27 @@ export default function GameCard({ game }: { game: Game }) {
     >
       <Link to={`/${game.id}/play`}>
         <div className="flex items-center justify-between">
-          <div className="flex flex-col items-start p-4">
-            <div className="flex gap-2 mb-2">
-              <strong className="shrink-0">{game.title}</strong>
+          <div className="flex flex-col items-start p-4 min-w-0">
+            <div className="flex gap-2 mb-2 w-full">
+              <strong
+                className="whitespace-nowrap min-w-0 overflow-hidden overflow-ellipsis"
+                title={game.title}
+              >
+                {game.title}
+              </strong>
               <div className="flex py-0.5">
                 <div className="border-l border-gray-200" />
               </div>
-              <p className="text-sm text-gray-500 flex items-center shrink-0">
-                {numRounds} {numRounds > 1 ? "rounds" : "round"}
+              <p className="shrink-0 text-sm text-gray-500 flex items-center">
+                {numRounds} {numRounds === 1 ? "round" : "rounds"}
               </p>
             </div>
-            <p className="text-sm">by {game.author}</p>
+            <p
+              className="w-full text-sm text-left whitespace-nowrap overflow-hidden overflow-ellipsis"
+              title={game.author}
+            >
+              by {game.author}
+            </p>
           </div>
           <div className="flex items-center justify-center p-1 mr-2 rounded-full transition-colors group-hover:bg-blue-200">
             {/* Heroicon name: outline/chevron-right */}
@@ -35,7 +45,10 @@ export default function GameCard({ game }: { game: Game }) {
               strokeWidth={1.5}
               stroke="currentColor"
               className="w-6 h-6 text-blue-500 group-hover:text-blue-600"
+              role="img"
+              aria-labelledby="start-title"
             >
+              <title id="start-title">Play game</title>
               <path
                 strokeLinecap="round"
                 strokeLinejoin="round"
