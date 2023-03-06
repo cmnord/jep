345bd83599dfa2ce847dd985748bb4e6cc66a2d0
Claire Nord <cnord@alum.mit.edu>
2023-03-06 12:55:02 -0800

    app components board category: switch react-textfit for useFitText
    React-textfit seems to be unmaintained and was not compatible with React v18.

--- app/components/board/category.tsx
+++ app/components/board/category.tsx
@@ -1,14 +1,17 @@
-import { Textfit } from "react-textfit";
+import useFitText from "use-fit-text";
 
 export function Category({ category }: { category: string }) {
+  const { fontSize, ref } = useFitText({ logLevel: "info", maxFontSize: 300 });
+
   return (
     <th className="p-4 h-full bg-blue-1000 border-black border-8 border-b-12">
-      <Textfit
+      <p
+        style={{ fontSize }}
+        ref={ref}
         className="w-full h-20 flex items-center justify-center font-bold font-impact uppercase text-shadow-md"
-        mode="multi"
       >
         {category}
-      </Textfit>
+      </p>
     </th>
   );
 }
