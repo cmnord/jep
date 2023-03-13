9a58a7d60af4a67ceab912785bc0e03aa40b81cf
Claire Nord <cnord@alum.mit.edu>
2023-03-13 16:26:31 -0700

    cypress: init cypress with basic settings
    Copied most of the config from https://github.com/remix-run/indie-stack

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,20 @@ module.exports = {
   extends: [
     "@remix-run/eslint-config",
     "@remix-run/eslint-config/node",
+    "@remix-run/eslint-config/jest-testing-library",
     "plugin:react-hooks/recommended",
     "prettier",
   ],
+  env: {
+    "cypress/globals": true,
+  },
+  plugins: ["cypress"],
+  // We're using vitest which has a very similar API to jest
+  // (so the linting plugins work nicely), but we have to
+  // set the jest version explicitly.
+  settings: {
+    jest: {
+      version: 28,
+    },
+  },
 };
