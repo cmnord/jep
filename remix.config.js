f3d889b79230c4ba35435622ee4fb655d2374af3
Claire Nord <cnord@alum.mit.edu>
2023-03-15 13:42:44 -0700

    remix.config.js: only use server.js on Vercel, add cache
    Copy some settings from indie-stack for remix.config.js.

--- remix.config.js
+++ remix.config.js
@@ -1,10 +1,6 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-  // When running locally in development mode, we use the built in remix
-  // server. This does not understand the vercel lambda module format,
-  // so we default back to the standard build output.
-  server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
-  ignoredRouteFiles: ["**/.*"],
+  cacheDirectory: "./node_modules/.cache/remix",
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "api/index.js",
@@ -13,5 +9,10 @@ module.exports = {
     unstable_tailwind: true,
     v2_routeConvention: true,
   },
+  ignoredRouteFiles: ["**/.*", "**/*.test.{js,jsx,ts,tsx}"],
+  // When running locally in development mode, we use the built in remix
+  // server. This does not understand the vercel lambda module format,
+  // so we default back to the standard build output.
+  server: process.env.VERCEL ? "./server.js" : undefined,
   serverDependenciesToBundle: ["nanoid"],
 };
