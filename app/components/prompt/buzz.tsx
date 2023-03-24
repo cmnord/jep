54f4dd1bc39856d9c5d95d53da456a03b20aae9a
Claire Nord <cnord@alum.mit.edu>
2023-03-24 10:05:36 -0700

    app components prompt buzz: show player icon on buzz only
    Sort the buzzes from fastest to slowest instead of showing the number of
    milliseconds per buzz.

--- app/components/prompt/buzz.tsx
+++ app/components/prompt/buzz.tsx
@@ -1,18 +1,16 @@
 import classNames from "classnames";
 import type { Player } from "~/engine";
 import { CANT_BUZZ_FLAG, CLUE_TIMEOUT_MS } from "~/engine";
-import { stringToHslColor } from "~/utils/utils";
+import { PlayerIcon } from "../player";
 
-function durationMessage(durationMs?: number) {
+function showBuzz(durationMs?: number) {
   switch (durationMs) {
     case undefined:
-      return "--";
     case CANT_BUZZ_FLAG:
-      return "cannot buzz";
     case CLUE_TIMEOUT_MS + 1:
-      return "timed out";
+      return false;
     default:
-      return durationMs + "ms";
+      return true;
   }
 }
 
@@ -25,36 +23,31 @@ const formatter = Intl.NumberFormat("en-US", {
 
 function Buzz({
   player,
-  durationMs,
   wonBuzz,
   clueValue,
 }: {
-  player?: Player;
-  durationMs?: number;
+  player: Player;
   wonBuzz: boolean;
   clueValue: number;
 }) {
-  const color = player ? stringToHslColor(player.userId) : "gray";
-  const durationMsg = durationMessage(durationMs);
   const clueValueStr = clueValue ? formatter.format(clueValue) : undefined;
 
   return (
-    <div
-      className="relative px-2 py-1 flex flex-col items-center justify-center text-white text-shadow"
-      style={{ color }}
-    >
+    <div>
       {wonBuzz && clueValueStr !== undefined ? (
         <span
-          className={classNames("absolute -top-5 font-bold animate-bounce", {
-            "text-green-300": clueValue >= 0,
-            "text-red-300": clueValue < 0,
-          })}
+          className={classNames(
+            "absolute -top-5 font-bold animate-bounce text-shadow",
+            {
+              "text-green-300": clueValue >= 0,
+              "text-red-300": clueValue < 0,
+            }
+          )}
         >
           {clueValueStr}
         </span>
       ) : null}
-      <div className="font-bold">{player?.name ?? "Unknown player"}</div>
-      <div>{durationMsg}</div>
+      <PlayerIcon player={player} />
     </div>
   );
 }
@@ -67,16 +60,17 @@ export function Buzzes({
   winningBuzzer,
   buzzCorrect,
 }: {
-  buzzes?: Map<string, number>;
+  buzzes: Map<string, number>;
   clueValue: number;
   players: Map<string, Player>;
   showWinner: boolean;
   winningBuzzer?: string;
   buzzCorrect: boolean;
 }) {
-  // sort players by buzz time
-  const sortedPlayers = Array.from(players.entries()).sort(
-    ([aUserId], [bUserId]) => {
+  // sort buzzes by time
+  const sortedBuzzes = Array.from(buzzes.entries())
+    .filter(([, b]) => showBuzz(b))
+    .sort(([aUserId], [bUserId]) => {
       const aDurationMs = buzzes?.get(aUserId);
       const bDurationMs = buzzes?.get(bUserId);
       if (aDurationMs === undefined) {
@@ -85,20 +79,23 @@ export function Buzzes({
         return -1;
       }
       return aDurationMs - bDurationMs;
-    }
-  );
+    });
+  const sortedPlayers = sortedBuzzes
+    .map(([userId]) => players.get(userId))
+    .filter((p): p is Player => p !== undefined);
 
   return (
-    <div className="flex gap-4 w-full overflow-x-scroll">
-      {sortedPlayers.map(([userId, player], i) => (
-        <Buzz
-          key={i}
-          player={player}
-          durationMs={buzzes?.get(userId)}
-          wonBuzz={winningBuzzer === userId && showWinner}
-          clueValue={buzzCorrect ? clueValue : -1 * clueValue}
-        />
-      ))}
+    <div className="relative">
+      <div className="flex gap-4 h-8 m-2 w-full overflow-x-scroll">
+        {sortedPlayers.map((player, i) => (
+          <Buzz
+            key={i}
+            player={player}
+            wonBuzz={winningBuzzer === player.userId && showWinner}
+            clueValue={buzzCorrect ? clueValue : -1 * clueValue}
+          />
+        ))}
+      </div>
     </div>
   );
 }
