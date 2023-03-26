014f73ab5e67d6be2f09c5ee78ea820cd4f1a7e0
Claire Nord <cnord@alum.mit.edu>
2023-03-25 20:36:32 -0700

    app components preview: simplify after-game preview

--- app/components/preview/preview.tsx
+++ app/components/preview/preview.tsx
@@ -107,22 +107,23 @@ function BeforeGamePreview({
   );
 }
 
+// TODO: game replay
 function AfterGamePreview({ clue }: { clue?: Clue }) {
   return (
-    <div className="px-3 pt-3 sm:px-6 sm:pt-6 md:px-12 md:pt-12">
+    <div className="bg-slate-900 text-slate-200 px-3 pt-3 sm:px-6 sm:pt-6 md:px-12 md:pt-12">
       <h2 className="text-2xl font-semibold mb-4">Game over!</h2>
       {clue ? (
         <div className="flex flex-col gap-4 mb-6">
           <p className="mb-2">The final answer was:</p>
-          <blockquote className="relative border-l-4 pl-4 sm:pl-6 dark:border-slate-700">
-            <p className="text-slate-800 sm:text-xl uppercase font-bold">
+          <blockquote className="relative border-l-4 pl-4 sm:pl-6">
+            <p className="text-slate-100 sm:text-xl uppercase font-bold">
               {clue.clue}
             </p>
           </blockquote>
           <p>
             <em>
               What is{" "}
-              <span className="uppercase font-bold text-sm bg-cyan-200 rounded-md p-1.5">
+              <span className="uppercase font-bold text-sm bg-cyan-200 text-slate-900 rounded-md p-1.5">
                 {clue?.answer}
               </span>
               ?
@@ -130,8 +131,6 @@ function AfterGamePreview({ clue }: { clue?: Clue }) {
           </p>
         </div>
       ) : null}
-      <hr className="my-4" />
-      <h2 className="text-2xl font-semibold mb-4">Final scores:</h2>
     </div>
   );
 }
