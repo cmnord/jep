4f0274178274f1c70cc8e9139d575c77985ccf78
Claire Nord <cnord@alum.mit.edu>
2023-03-25 20:32:00 -0700

    app components preview: simplify rounds between 0 and R-1
    Where R is the total number of rounds.

--- app/models/mock.server.ts
+++ app/models/mock.server.ts
@@ -3,6 +3,16 @@ import { Convert } from "./convert.server";
 
 import type { Game } from "./game.server";
 
+/** Find the absolute path of the json directory.
+ *
+ * Note: Vercel doesn't include the json directory when using process.cwd() or
+ * path.join(). The workaround is to use __dirname and concatenate the json
+ * directory to it.
+ */
+const JSON_DIRECTORY = __dirname + "/../app/static";
+
+const MOCK_FILE_PATH = JSON_DIRECTORY + "/mock.jep.json";
+
 /** MOCK_GAME has two rounds. The first round has 1 category with 2 clues and the
  * second round has 1 category with 1 clue.
  */
@@ -39,14 +49,7 @@ export const MOCK_GAME: Game = {
 };
 
 export async function getMockGame(): Promise<Game> {
-  // Find the absolute path of the json directory
-  // Note: Vercel doesn't include the json directory when using process.cwd() or
-  // path.join(). The workaround is to use __dirname and concatenate the json
-  // directory to it.
-  const jsonDirectory = __dirname + "/../app/static";
-  // Read the json data file data.json
-  const fileContents = await readFile(jsonDirectory + "/mock.jep.json", "utf8");
-
+  const fileContents = await readFile(MOCK_FILE_PATH, "utf8");
   const game = Convert.toGame(fileContents);
   return { id: "mock", ...game };
 }
