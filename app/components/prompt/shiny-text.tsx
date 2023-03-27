ebda9ef62390e536c8a7c424395b622631ba1d30
Claire Nord <cnord@alum.mit.edu>
2023-03-27 15:18:43 -0700

    app components prompt: show final clue text + category
    Lift up useFitText to the div above ShinyText to avoid a maximum update depth
    error.

--- app/components/prompt/shiny-text.tsx
+++ app/components/prompt/shiny-text.tsx
@@ -1,17 +1,11 @@
-import useFitText from "use-fit-text";
-
 export default function ShinyText({ text }: { text: string }) {
-  const { ref, fontSize } = useFitText({ minFontSize: 20, maxFontSize: 600 });
-
   return (
     <div
       className="grid text-center font-black uppercase mx-auto"
-      ref={ref}
       style={{
         // a bit of a hack to keep the text from going to maxFontSize
         // (like 100vw + padding)
         maxWidth: "90%",
-        fontSize,
       }}
     >
       <div className="shiny-text shiny-text-bg">{text}</div>
