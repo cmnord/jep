1cafb1d700184aea562a64dbe502468fa7eb0778
Claire Nord <cnord@alum.mit.edu>
2023-04-10 19:37:30 -0700

    app components prompt: color clue timer red/green, count down
    Instead of counting down clue read time with the bottom timer, count down with
    the same ReadClueTimer. This and the green/read should make it clearer when you
    need to take action.

--- app/components/prompt/read-clue-timer.tsx
+++ app/components/prompt/read-clue-timer.tsx
@@ -5,20 +5,25 @@ import classNames from "classnames";
 export function ReadClueTimer({
   clueDurationMs,
   shouldAnimate,
+  wonBuzz,
 }: {
   clueDurationMs: number;
   shouldAnimate: boolean;
+  wonBuzz: boolean;
 }) {
   return (
     <div
       className={classNames("h-8 mt-2 self-start", {
         "w-0 bg-white": shouldAnimate,
-        "w-full bg-lime-500": !shouldAnimate,
+        "w-full": !shouldAnimate,
+        "bg-red-600 opacity-75": !shouldAnimate && !wonBuzz,
+        "bg-lime-500": !shouldAnimate && wonBuzz,
       })}
       style={{
         animation: shouldAnimate
           ? `${clueDurationMs / 1000}s linear 0s 1 growFromLeft forwards,
-             ${clueDurationMs / 1000}s linear 0s 1 turnGreen forwards`
+             ${clueDurationMs / 1000}s linear 0s 1 turnGreen forwards,
+             5s linear ${clueDurationMs / 1000}s 1 shrinkToLeft forwards`
           : "none",
       }}
     />
