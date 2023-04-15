3b2859d9d48ba05f598322292537097588135bbb
Claire Nord <cnord@alum.mit.edu>
2023-04-14 17:21:27 -0700

    prettierrc: sort tailwind css classes with prettier plugin

--- app/components/button.tsx
+++ app/components/button.tsx
@@ -32,15 +32,15 @@ export default function Button({
       disabled={disabled}
       onClick={onClick}
       className={classNames(
-        className ? className : "",
+        className ? className : null,
         "inline-flex w-full justify-center rounded-md border px-4 py-2 text-base font-medium shadow-sm transition-colors",
         "focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2",
         "sm:w-auto sm:text-sm",
-        "disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none disabled:cursor-not-allowed",
+        "disabled:cursor-not-allowed disabled:border-slate-200 disabled:bg-slate-50 disabled:text-slate-500 disabled:shadow-none",
         {
           "border-transparent bg-blue-600 text-white hover:bg-blue-700":
             type === "primary",
-          "border-blue-600 bg-white text-blue-600 hover:bg-slate-100 hover:text-blue-700 hover:border-blue-700":
+          "border-blue-600 bg-white text-blue-600 hover:border-blue-700 hover:bg-slate-100 hover:text-blue-700":
             type === "default",
         }
       )}
