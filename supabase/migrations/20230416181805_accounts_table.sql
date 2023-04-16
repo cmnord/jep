c08a6e972b073c9bf311179add032953c8ad8d50
Claire Nord <cnord@alum.mit.edu>
2023-04-16 11:21:08 -0700

    app models: add accounts model to database
    Cannot name the table `users` because of
    https://github.com/supabase/supabase/issues/1479

---
+++ supabase/migrations/20230416181805_accounts_table.sql
@@ -0,0 +1,30 @@
+create table "public"."accounts" (
+    "id" uuid not null,
+    "created_at" timestamp with time zone not null default now(),
+    "email" character varying not null
+);
+
+
+alter table "public"."accounts" enable row level security;
+
+CREATE UNIQUE INDEX accounts_pkey ON public.accounts USING btree (id);
+
+alter table "public"."accounts" add constraint "accounts_pkey" PRIMARY KEY using index "accounts_pkey";
+
+alter table "public"."accounts" add constraint "accounts_id_fkey" FOREIGN KEY (id) REFERENCES auth.users(id) not valid;
+
+alter table "public"."accounts" validate constraint "accounts_id_fkey";
+
+create policy "User can only read their own profile"
+on "public"."accounts"
+as permissive
+for select
+to authenticated
+using ((id = auth.uid()));
+
+create policy "Enable insert for un-authenticated users only"
+on "public"."accounts"
+as permissive
+for insert
+to anon
+with check (true);
