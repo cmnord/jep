b4e18055c34a85a844404c59f310a8fb91fbf15f
Claire Nord <cnord@alum.mit.edu>
2023-04-23 13:53:33 -0700

    app routes: add user by logged-in user ID if present
    If a user has a logged-in session, add that ID to the game instead of the guest
    session.
    
    This means that if a user:
    - joins as a guest and then logs in
    - joins logged in and then logs out
    
    Then they'll have two users in the game. This is the simplest way to handle
    these situations for now.

--- app/routes/room.$roomName.tsx
+++ app/routes/room.$roomName.tsx
@@ -6,9 +6,11 @@ import GameComponent from "~/components/game";
 import { ActionType, GameEngineContext, useGameEngine } from "~/engine";
 import { isPlayerAction } from "~/engine/actions";
 import { isTypedRoomEvent } from "~/engine/room-event";
+import { getValidAuthSession } from "~/models/auth";
 import { getGame } from "~/models/game.server";
 import { createRoomEvent, getRoomEvents } from "~/models/room-event.server";
 import { getRoom } from "~/models/room.server";
+import { getUserByEmail } from "~/models/user";
 import { getOrCreateUserSession } from "~/session.server";
 import { SUPABASE_ANON_KEY, SUPABASE_URL } from "~/utils";
 import { getRandomName } from "~/utils/name";
@@ -37,24 +39,46 @@ export async function loader({ request, params }: LoaderArgs) {
     throw new Response("game not found", { status: 404 });
   }
 
-  const headers = new Headers();
-  const userId = await getOrCreateUserSession(request, headers);
+  const authSession = await getValidAuthSession(request);
+  const user = authSession
+    ? await getUserByEmail(authSession.email, authSession.accessToken)
+    : null;
 
   const roomEvents = await getRoomEvents(room.id);
-  const userInRoom = roomEvents
-    .filter(isTypedRoomEvent)
-    .find((e) => isPlayerAction(e) && e.payload.userId === userId);
+  const typedRoomEvents = roomEvents.filter(isTypedRoomEvent);
 
-  if (!userInRoom) {
+  const env = { SUPABASE_URL, SUPABASE_ANON_KEY };
+
+  // Add the user to the room if they aren't already in it.
+  // If they are logged in, their ID is their user ID.
+  // If they are a guest, their ID is their guest session ID.
+  if (user) {
+    const userId = user.id;
+    const userInRoom = typedRoomEvents.some(
+      (re) => isPlayerAction(re) && re.payload.userId === userId
+    );
+    if (!userInRoom) {
+      const joinEvent = await createRoomEvent(room.id, ActionType.Join, {
+        userId,
+        name: getRandomName(),
+      });
+      roomEvents.push(joinEvent);
+    }
+    return json({ room, roomName, game, roomEvents, userId, env });
+  }
+
+  const headers = new Headers();
+  const userId = await getOrCreateUserSession(request, headers);
+  const guestInRoom = typedRoomEvents.some(
+    (re) => isPlayerAction(re) && re.payload.userId === userId
+  );
+  if (!guestInRoom) {
     const joinEvent = await createRoomEvent(room.id, ActionType.Join, {
       userId,
       name: getRandomName(),
     });
     roomEvents.push(joinEvent);
   }
-
-  const env = { SUPABASE_URL, SUPABASE_ANON_KEY };
-
   return json({ room, roomName, game, roomEvents, userId, env }, { headers });
 }
 
