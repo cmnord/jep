e7288908c54aa4745b706e7a16d6391c4c0d9371
Claire Nord <cnord@alum.mit.edu>
2023-04-24 23:12:28 -0700

    app components icons: add Eye and EyeSlash

--- app/components/icons/index.tsx
+++ app/components/icons/index.tsx
@@ -1,4 +1,6 @@
+import { Eye } from "./eye";
+import { EyeSlash } from "./eye-slash";
 import { LoadingSpinner } from "./loading-spinner";
 import { QuestionMarkCircle } from "./question-mark-circle";
 
-export { QuestionMarkCircle, LoadingSpinner };
+export { QuestionMarkCircle, LoadingSpinner, Eye, EyeSlash };
