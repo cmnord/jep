c40a33e32a1f181aaa13297900763ad153966854
Claire Nord <cnord@alum.mit.edu>
2023-05-04 16:06:56 -0700

    app: fix up all icons to have Heroicon names, solid not outline

--- app/components/copy-link-button.tsx
+++ app/components/copy-link-button.tsx
@@ -36,21 +36,20 @@ export default function CopyLinkButton({
           }, 100);
         }}
       >
+        {/* Heroicon name: solid/link */}
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          fill="none"
           viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
+          fill="currentColor"
           className="h-6 w-6"
           role="img"
           aria-labelledby="copy-title"
         >
           <title id="copy-title">Copy link</title>
           <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244"
+            fillRule="evenodd"
+            d="M19.902 4.098a3.75 3.75 0 00-5.304 0l-4.5 4.5a3.75 3.75 0 001.035 6.037.75.75 0 01-.646 1.353 5.25 5.25 0 01-1.449-8.45l4.5-4.5a5.25 5.25 0 117.424 7.424l-1.757 1.757a.75.75 0 11-1.06-1.06l1.757-1.757a3.75 3.75 0 000-5.304zm-7.389 4.267a.75.75 0 011-.353 5.25 5.25 0 011.449 8.45l-4.5 4.5a5.25 5.25 0 11-7.424-7.424l1.757-1.757a.75.75 0 111.06 1.06l-1.757 1.757a3.75 3.75 0 105.304 5.304l4.5-4.5a3.75 3.75 0 00-1.035-6.037.75.75 0 01-.354-1z"
+            clipRule="evenodd"
           />
         </svg>
         {text ? <span className="pl-2 pr-1 text-sm">{text}</span> : null}
--- app/routes/profile/game-info.tsx
+++ app/routes/profile/game-info.tsx
@@ -15,23 +15,22 @@ const formatter = new Intl.DateTimeFormat("en-US", {
   day: "numeric",
 });
 
+/** Heroicon name: solid/ellipsis-horizontal */
 function EllipsisIcon() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      fill="none"
       viewBox="0 0 24 24"
-      strokeWidth={1.5}
-      stroke="currentColor"
+      fill="currentColor"
       className="h-6 w-6"
       role="img"
       aria-labelledby="more-actions-title"
     >
       <title id="more-actions-title">More actions</title>
       <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M6.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM12.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM18.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
+        fillRule="evenodd"
+        d="M4.5 12a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zm6 0a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zm6 0a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0z"
+        clipRule="evenodd"
       />
     </svg>
   );
