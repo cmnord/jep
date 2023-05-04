c53c1e27e2ebe5a4a158394f3c9e5f88dfd43852
Claire Nord <cnord@alum.mit.edu>
2023-05-03 19:56:34 -0700

    app models report: add reports table and insert on POST /report

---
+++ app/models/report.server.ts
@@ -0,0 +1,31 @@
+import { db } from "~/db.server";
+import type { Database } from "~/models/database.types";
+
+type ReportTable = Database["public"]["Tables"]["reports"];
+
+/* Writes */
+
+export async function insertReport(
+  gameId: string,
+  reason: string,
+  userId?: string
+) {
+  const { data, error } = await db
+    .from<"reports", ReportTable>("reports")
+    .insert<ReportTable["Insert"]>({
+      created_by: userId,
+      game_id: gameId,
+      reason,
+    })
+    .select();
+
+  if (error !== null) {
+    throw new Error(error.message);
+  }
+
+  const report = data.at(0);
+  if (!report) {
+    throw new Error("report not created");
+  }
+  return report.id;
+}
