c9d995d44514fd05a945a15cd1606316d10d2c93
Claire Nord <cnord@alum.mit.edu>
2023-08-20 21:53:31 -0700

    cypress: tweak linter config
    See https://github.com/cypress-io/cypress/issues/26930

--- cypress/.eslintrc.js
+++ cypress/.eslintrc.js
@@ -1,6 +1,8 @@
+/** @type {import('eslint').Linter.Config} */
 module.exports = {
   parserOptions: {
     tsconfigRootDir: __dirname,
     project: "./tsconfig.json",
   },
+  ignorePatterns: ["*.js"],
 };
